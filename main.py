5e513f1a618cb08162c74b6fef96935a19f6db54
Aarnav Verma <your.email@example.com>
2025-11-02 15:35:21 -0500

    Update port binding

--- main.py
+++ main.py
@@ -101,5 +101,5 @@ def health(): return {"status": "ok"}
 
 if __name__ == "__main__":
     import uvicorn, os
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", "8000")), reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", "10000")), reload=True)
 
